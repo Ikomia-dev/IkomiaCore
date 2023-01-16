216deac94927f925764ea564b286b9e50ac1e9b7
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-01-16 15:55:39 +0100

    Renaming PyDataIO module

--- PyDataIO/PyDataIODocString.hpp
+++ PyDataIO/PyDataIODocString.hpp
@@ -7,69 +7,72 @@ constexpr auto _moduleDocString = "Module providing I/O functions to manage imag
 //----- CDataImageIO -----//
 //------------------------//
 constexpr auto _dataImageIODocString =
-        "Class to handle image file formats. It is mainly backed by OpenCV.\n";
+        "Class to handle image file formats. It is mainly backed by OpenCV.\n\n";
 
 constexpr auto _ctorDataImageIO =
         "Construct object to load/write image in basic formats.\n\n"
-        "Args:\n"
+        "Args:\n\n"
         "   arg1 : self\n\n"
-        "   arg2 : path to image file\n";
+        "   arg2 : path to image file\n\n";
 
 constexpr auto _readDataImageDocString =
         "Read image from basic file format (jpg, png, tif, bmp, npz, dicom...). You can consult OpenCV documentation to have a more complete list of compatible formats.\n\n"
-        "Returns:\n"
-        "   numpy array: image loaded\n";
+        "Returns:\n\n"
+        "   numpy array: image loaded\n\n";
 
 constexpr auto _writeDataImageDocString =
         "Write image to basic file format (jpg, png, tif, bmp...). You can consult OpenCV documentation to have a more complete list of compatible formats. Note: DICOM and NPZ formats are not available for writing.\n\n"
-        "Args:\n"
-        "   numpy array: image to save\n";
+        "Args:\n\n"
+        "   numpy array: image to save\n\n";
 
 constexpr auto _isImageFormatDocString =
         "Static method to check if the given extension is a compatible image format\n\n"
-        "Args:\n"
-        "   str: file extension (including the dot)\n";
+        "Args:\n\n"
+        "   str: file extension (including the dot)\n\n";
 
 //------------------------//
 //----- CDataVideoIO -----//
 //------------------------//
 constexpr auto _dataVideoIODocString =
-        "Class to handle video file formats. It is mainly backed by OpenCV.\n";
+        "Class to handle video file formats. It is mainly backed by OpenCV.\n\n";
 
 constexpr auto _ctorDataVideoIO =
         "Construct object to load/write video in basic formats.\n\n"
-        "Args:\n"
+        "Args:\n\n"
         "   arg1 : self\n\n"
-        "   arg2 : path to video file\n";
+        "   arg2 : path to video file\n\n";
 
 constexpr auto _readDataVideoDocString =
         "Read next video frame from basic file format (avi, mp4, webm). You can consult OpenCV documentation to have a more complete list of compatible formats.\n\n"
-        "Returns:\n"
-        "   numpy array: frame read\n";
+        "Returns:\n\n"
+        "   numpy array: frame read\n\n";
 
 constexpr auto _writeDataVideo1DocString =
         "Write video frame to basic file format (avi, mp4). You can consult OpenCV documentation to have a more complete list of compatible formats.\n\n"
-        "Args:\n"
-        "   numpy array: frame to append\n";
+        "Args:\n\n"
+        "   numpy array: frame to append\n\n";
 
 constexpr auto _writeDataVideo2DocString =
         "Write video frame to basic file format (avi, mp4). You can consult OpenCV documentation to have a more complete list of compatible formats.\n\n"
-        "Args:\n"
-        "   numpy array: frame to append\n"
-        "   str: path to video file";
+        "Args:\n\n"
+        "   numpy array: frame to append\n\n"
+        "   str: path to video file\n\n";
 
 constexpr auto _stopReadDocString =
-        "Stop reading process.\n";
+        "Stop reading process.\n\n";
 
 constexpr auto _stopWriteDocString =
-        "Stop writing process.\n";
+        "Stop writing process.\n\n";
 
 constexpr auto _waitWriteFinishedDocString =
-        "Wait for writing process to finish.\n";
+        "Wait for writing process to finish.\n\n";
 
 constexpr auto _isVideoFormatDocString =
         "Static method to check if the given extension is a compatible video format\n\n"
-        "Args:\n"
-        "   str: file extension (including the dot)\n";
+        "Args:\n\n"
+        "   str: file extension (including the dot)\n\n"
+        "   bool: if true check only video format, otherwise check also image format for time sequence\n\n"
+        "Returns:\n\n"
+        "   bool: True if compatible, False otherwise\n\n";
 
 #endif // PYDATAIODOCSTRING_HPP
