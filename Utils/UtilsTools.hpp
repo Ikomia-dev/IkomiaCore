fd61e1a80b581dba5cea0dbf09e6a66e78378d99
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2024-06-24 16:21:23 +0200

    Chore: set version 0.11.1

--- Utils/UtilsTools.hpp
+++ Utils/UtilsTools.hpp
@@ -529,11 +529,11 @@ namespace Ikomia
             }
             inline std::string  getCurrentVersionNumber()
             {
-                return "0.11.0";
+                return "0.11.1";
             }
             inline std::string  getCurrentVersionName()
             {
-                return "0.11.0";
+                return "0.11.1";
             }
             inline std::string  getIkomiaLibFolder()
             {
@@ -1354,7 +1354,7 @@ namespace Ikomia
             }
             inline std::string  getCurrentApiVersion()
             {
-                return "0.11.0";
+                return "0.11.1";
             }
             inline std::string  getModelHubUrl()
             {
